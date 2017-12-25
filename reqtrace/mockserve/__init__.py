3e73efc5fd1635560fe871e354cfa0704ec695d6
podhmo <ababjam61+github@gmail.com>
2017-12-26 06:46:25 +0900

    support 3.5

--- reqtrace/mockserve/__init__.py
+++ reqtrace/mockserve/__init__.py
@@ -1,8 +1,20 @@
-from wsgiref.simple_server import make_server
+from wsgiref.simple_server import make_server as _make_server
+from wsgiref.simple_server import WSGIServer as _WSGIServer
 from ..util import find_freeport
 from .core import create_app  # noqa
+
+if hasattr(_WSGIServer, "__enter__"):
+    WSGIServer = _WSGIServer
+else:
+
+    class WSGIServer(_WSGIServer):
+        def __enter__(self):
+            return self
+
+        def __exit__(self, typ, val, tb):
+            self.server_close()
 
 
 def create_server(app, host='', port=None):
     port = port or find_freeport()
-    return make_server(host, port, app)
+    return _make_server(host, port, app, server_class=WSGIServer)
