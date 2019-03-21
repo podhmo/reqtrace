a4305b129d4aa127d2716a511f7763194be8bc90
podhmo <ababjam61+github@gmail.com>
2019-03-22 00:37:05 +0900

    0.0.0

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
 import os
 
 from setuptools import setup, find_packages
+
 here = os.path.abspath(os.path.dirname(__file__))
 try:
-    with open(os.path.join(here, 'README.rst')) as f:
+    with open(os.path.join(here, "README.rst")) as f:
         README = f.read()
-    with open(os.path.join(here, 'CHANGES.txt')) as f:
+    with open(os.path.join(here, "CHANGES.txt")) as f:
         CHANGES = f.read()
 except IOError:
-    README = CHANGES = ''
+    README = CHANGES = ""
 
-install_requires = [
-    "requests",
-    "httplib2",
-    "magicalimport",
-]
+install_requires = ["requests", "httplib2", "magicalimport"]
 
-googleapi_extras = [
-    "google-api-python-client",
-]
+googleapi_extras = ["google-api-python-client"]
 
 docs_extras = []
 
@@ -27,31 +22,31 @@ tests_require = []
 testing_extras = tests_require + []
 
 setup(
-    name='reqtrace',
-    version='0.0.0',
-    description='-',
-    long_description=README + '\n\n' + CHANGES,
+    name="reqtrace",
+    version="0.0.0",
+    description="tracing request. (don't use this in production)",
+    long_description=README + "\n\n" + CHANGES,
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
-    keywords='',
-    author="",
-    author_email="",
-    url="",
+    keywords="requests, httplib2, urllib",
+    author="podhmo",
+    author_email="ababjam61@gmail.com",
+    url="https://github.com/podhmo/reqtrace",
     packages=find_packages(exclude=["reqtrace.tests"]),
     include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
     extras_require={
-        'testing': testing_extras,
-        'docs': docs_extras,
-        'googleapi': googleapi_extras,
+        "testing": testing_extras,
+        "docs": docs_extras,
+        "googleapi": googleapi_extras,
     },
     tests_require=tests_require,
     test_suite="reqtrace.tests",
     entry_points="""
-[console_scripts]
-reqtrace=reqtrace.commands.reqtrace:main
-"""
+    [console_scripts]
+    py-reqtrace=reqtrace.commands.reqtrace:main
+    """,
 )
