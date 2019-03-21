21838f5c1ea140601f160a0716e88b15105fed10
podhmo <ababjam61+github@gmail.com>
2019-03-22 00:53:45 +0900

    feat: add --enable option, default is enable all

--- reqtrace/commands/reqtrace.py
+++ reqtrace/commands/reqtrace.py
@@ -4,12 +4,14 @@ import os.path
 import shutil
 import uuid
 import argparse
+from functools import partial
 from importlib.util import spec_from_file_location
 from importlib.machinery import SourceFileLoader
 from magicalimport import import_symbol
 from reqtrace.tracelib.hooks import trace
 from reqtrace.tracelib.requests import monkeypatch as monkeypatch_requests
 from reqtrace.tracelib.httplib2 import monkeypatch as monkeypatch_httplib2
+
 logger = logging.getLogger(__name__)
 
 
@@ -39,16 +41,33 @@ def main():
     parser.add_argument("command")
     parser.add_argument("--outdir", default="./roundtrips")
     parser.add_argument("--with-uid", action="store_true")
-    parser.add_argument("--logging", choices=list(logging._nameToLevel.keys()), default="INFO")
+    parser.add_argument(
+        "--logging", choices=list(logging._nameToLevel.keys()), default="INFO"
+    )
+    parser.add_argument(
+        "--enable",
+        dest="enables",
+        action="append",
+        choices=["requests", "httplib2"],
+        default=None,
+    )
 
     args, extras = parser.parse_known_args()
     logging.basicConfig(level=args.logging)
     if args.with_uid:
         args.outdir = os.path.join(args.outdir, uuid.uuid4().hex[:8])
+
     logger.info("outdir is %s", args.outdir)
     usepip = args.command.strip() == "pip"
 
     hook = trace(dirpath=args.outdir)
-    monkeypatch_requests(on_request=noop, on_response=hook, pip=usepip)
-    monkeypatch_httplib2(on_request=noop, on_response=hook)
+    enables = args.enables or ["requests", "httplib2"]
+
+    for module_name in enables:
+        logger.info("monkey patching for %s", module_name)
+        monkeypatch = globals()[f"monkeypatch_{module_name}"]
+        if module_name == "requests":
+            monkeypatch = partial(monkeypatch, pip=usepip)
+        monkeypatch(on_request=noop, on_response=hook)
+
     call_file(args.command, extras)
